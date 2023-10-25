6f907c799494b183a27da3373344a6cee5ccec8d
Taukon <83957880+Taukon@users.noreply.github.com>
2023-10-26 00:42:07 +0900

    fix winapi

--- src/main/ipcHandle/shareApp/win32/win32lib.js
+++ src/main/ipcHandle/shareApp/win32/win32lib.js
@@ -1,8 +1,1 @@
-// const helloPath = path.join(__dirname, '../build/Release/hello.node');
-// const helloPath = path.join(__dirname, '../../../../../build/Release/hello.node');
-// // const helloPath = path.join(__dirname, '../bin/win32-x64-116/shareDesktop.node');
-
-// export const hello = __non_webpack_require__(helloPath);
 export const winSimulator = __non_webpack_require__(`./winSimulator.node`);
-
-// export const hello = require('electron').remote.require(helloPath);
