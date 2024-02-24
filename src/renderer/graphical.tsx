d7370d2ab9bb49b299b85ca7c37db70ad357ce36
Taukon <83957880+Taukon@users.noreply.github.com>
2024-02-25 05:29:07 +0900

    fix

--- src/renderer/graphical.tsx
+++ src/renderer/graphical.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { FileShare } from './components/fileShare';
-import { HostScreen } from './components/hostScreen';
+import { FileShare } from './components/FileShare';
+import { HostScreen } from './components/HostScreen';
 import { impromptu } from '.';
 import "./index.css";
 
